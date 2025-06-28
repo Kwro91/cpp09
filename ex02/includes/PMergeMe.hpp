0cd524a4c40080b9eff5734dd7fce960e8ad0925
Benjamin SALORT <besalort@f1r6s5.clusters.42paris.fr>
2025-06-28 18:22:21 +0200

    Refonte complete + fonctionnelle

--- ex02/includes/PMergeMe.hpp
+++ ex02/includes/PMergeMe.hpp
@@ -1,60 +1,48 @@
-#ifndef PMERGERME_HPP
+#ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
 #include <iostream>
-#include <list>
 #include <vector>
-#include <string>
-#include <cctype>
-#include <sstream>
-#include <stdlib.h>
-#include <ctime>
-#include <utility>
 #include <deque>
+#include <ctime>
 #include <exception>
+#include <climits>
+#include <stdexcept>
+#include <algorithm>
+#include <iomanip>
 
 class PMergeMe{
 	private:
-		int	_nbEntry;
-		std::string							_entry;
-		std::string							_sorted;
-		std::deque<int>						_dNumbers;
-		std::vector<int>					_vNumbers;
-		std::vector<std::pair<int, int>>	_vPairs;
-		std::deque<std::pair<int, int>>		_dPairs;
-		double								_dTime;
-		double								_vTime;
-		
+		std::deque<int>		_dNumbers;
+		std::vector<int>	_vNumbers;
+
+		void				check(char **av);
+		int					ftStoi(const char *str);
+
+		////////////////////_VECTOR_////////////////////////////////
+		void				vFordJohnson(std::vector<int> &v);
+		void				vBinaryInsert(std::vector<int> &sorted, int value); //Where to insert
+		std::vector<int>	vGenerateJacobsthalIndices(int n); //Which to insert
+		/////////////////////_DEQUE_////////////////////////////////
+		void				dFordJohnson(std::deque<int> &d);
+		void				dBinaryInsert(std::deque<int> &sorted, int value);
+		std::deque<int>		dGenerateJacobsthalIndices(int n);
 
 	public:
 		PMergeMe();
 		~PMergeMe();
 		PMergeMe(const PMergeMe &p);
 		PMergeMe	&operator=(const PMergeMe &p);
-		PMergeMe(const std::string &entry, int nbEntry);
 
-		const std::string		&getEntry() const;
-		const std::string		&getSorted() const;
-		const int				&getNbEntry() const;
-		const double			&getDTime() const;
-		const double			&getVTime() const;
-		const std::deque<int>	&getDNumbers() const;
-		const std::vector<int>	&getVNumbers() const;
+		void				sortFJ(char **av);
 
-		void	checkEntry(const std::string &entry);
-		void	fillContainer(const std::string &entry);
-
-		void	vMergeList(std::vector<int> &v);
-		void	dMergeList(std::deque<int> &d);
-
-
-		//////////////////////////////_EXCEPTION_//////////////////////////////
+		///////////////////_EXCEPTION_//////////////////////////////
 
 
 		class InvalidChar : public std::exception {
 			public :
 				virtual const char* what() const throw() {
-					return ("Error: invalid character. Please use only '0123456789 '.");
+					return ("Error: invalid character. Please use only: '0123456789 '");
 				}
 		};
 
--- ex02/srcs/PMergeMe.cpp
+++ ex02/srcs/PMergeMe.cpp
@@ -1,93 +1,250 @@
 #include "../includes/PMergeMe.hpp"
 
-PMergeMe::PMergeMe() : _entry("default"), _sorted("default") {}
+PMergeMe::PMergeMe() {}
 
-PMergeMe::~PMergeMe(){}
-
-PMergeMe::PMergeMe(const std::string &entry, int nbEntry) {
-	_entry = entry;
-	_nbEntry = nbEntry;
-}
+PMergeMe::~PMergeMe() {}
 
 PMergeMe::PMergeMe(const PMergeMe &p) {
-	(void)p;
+	_vNumbers.clear();
+    _vNumbers.insert(_vNumbers.end(), p._vNumbers.begin(), p._vNumbers.end());
+    _dNumbers.clear();
+    _dNumbers.insert(_dNumbers.end(), p._dNumbers.begin(), p._dNumbers.end());
 }
 
 PMergeMe	&PMergeMe::operator=(const PMergeMe &p) {
-	(void)p;
+    if (this == &p)
+		return (*this);
+    _vNumbers.clear();
+    _vNumbers.insert(_vNumbers.end(), p._vNumbers.begin(), p._vNumbers.end());
+    _dNumbers.clear();
+    _dNumbers.insert(_dNumbers.end(), p._dNumbers.begin(), p._dNumbers.end());
+    return (*this);
 }
 
-const std::string	&PMergeMe::getEntry() const {return (_entry);}
-const std::string	&PMergeMe::getSorted() const {return (_sorted);}
-const int			&PMergeMe::getNbEntry() const {return (_nbEntry);}
-const double		&PMergeMe::getDTime() const {return (_dTime);}
-const double		&PMergeMe::getVTime() const {return (_vTime);}
-const std::deque<int>	&PMergeMe::getDNumbers() const {return (_dNumbers);}
-const std::vector<int>	&PMergeMe::getVNumbers() const {return (_vNumbers);}
+int	PMergeMe::ftStoi(const char *str){
+	int result = 0;
 
-void	PMergeMe::checkEntry(const std::string &entry) {
-	std::string validChar = "0123456789 ";
-
-	for (size_t i = 0; i < entry.size(); i++)
+	while (*str == ' ' || *str == '\t')
+		str++;
+	if (*str == '+')
+		str++;
+	else if (*str == '-')
+		throw (NegativeInt());
+	
+	while (*str)
 	{
-		if (validChar.find(entry[i] == std::string::npos))
-			throw InvalidChar();
+		if (*str < '0' || *str > '9')
+			throw (InvalidChar());	
+		int digit = *str - '0';
+		if (result > (INT_MAX - digit) / 10)
+			throw (std::out_of_range("Error: overflow detected."));
+		result = result * 10 + digit;
+		str++;
+	}
+	return (result);
+}
+
+void	PMergeMe::check(char **av){
+	for (int i = 1; av[i]; i++)
+	{
+		int value = ftStoi(av[i]);
+		_vNumbers.push_back(value);
+		_dNumbers.push_back(value);
 	}
 }
 
-void	PMergeMe::fillContainer(const std::string &entry) {
-	int	cnt = 0;
-	int	tmp = 0;
-	std::vector<std::pair<int, int>> vTmp;
-	clock_t timeUsedS;
-	clock_t timeUsedE;
-	// (void)_nbEntry;
+void	PMergeMe::sortFJ(char **av) {
+	try {
+		check(av);
 
-	checkEntry(entry);
-	for (size_t i = 0; i < entry.size(); i++)
+		std::cout << "Before FJ: ";
+		for (size_t i = 0; i < _vNumbers.size(); i++)
+			std::cout << _vNumbers[i] << " ";
+		std::cout << std::endl;
+		
+		//Vector
+		std::clock_t start = std::clock();
+		vFordJohnson(_vNumbers);
+		std::clock_t stop = std::clock();
+		double vTime = ((double)(stop - start) / CLOCKS_PER_SEC * 1000000.0);
+
+		std::cout << "After: ";
+		for (size_t i = 0; i < _vNumbers.size(); i++)
+			std::cout << _vNumbers[i] << " ";
+		std::cout << std::endl;
+
+		std::cout << std::fixed << std::setprecision(5); //Change la precision des decimaux a l'affichage.
+		std::cout << "Time to process a range of " << _vNumbers.size() << " elements with std::vector : " << vTime << " us." << std::endl;
+		
+		//Deque
+		start = std::clock();
+		dFordJohnson(_dNumbers);
+		stop = std::clock();
+		double dTime = ((double)(stop - start) / CLOCKS_PER_SEC * 1000000.0);
+		std::cout << "Time to process a range of " << _dNumbers.size() << " elements with std::deque : " << dTime << " us." << std::endl;
+	
+	} catch (std::exception &e)
 	{
-		if (i != 0 && isdigit(entry[i - 1]) && entry[i] == ' ')
+		std::cerr << e.what() << std::endl;
+	}
+
+}
+
+////////////////////////////////VECTOR///////////////////////////////////
+
+std::vector<int> PMergeMe::vGenerateJacobsthalIndices(int n){
+	std::vector<int> tmp;
+	tmp.push_back(0);
+	tmp.push_back(1);
+	while (tmp[tmp.size() - 1] < n)
+	{
+		int next = tmp[tmp.size() - 1] * 2 + tmp[tmp.size() - 2];
+		tmp.push_back(next);
+	}
+	tmp.pop_back();
+
+	std::vector<int> index;
+	for (size_t i = 0; i < tmp.size(); i++)
+		index.push_back(tmp[i]);
+	
+	std::vector<bool> used(n, false);
+	for (size_t i = 0; i < tmp.size(); i++)
+		if (index[i] < n)
+			used[index[i]] = true;
+
+	for (int i = 0; i < n; i++)
+		if (!used[i])
+			index.push_back(i);
+	return (index);
+}
+
+void	PMergeMe::vBinaryInsert(std::vector<int> &v, int value){
+	int left = 0, right = v.size();
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		if (v[mid] < value)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+	v.push_back(0);
+	for (int i = (v.size() - 1); i > left; --i)
+		v[i] =  v[i - 1];
+	v[left] = value;
+}
+
+void	PMergeMe::vFordJohnson(std::vector<int> &v){
+	int n = v.size();
+	if (n <= 1)
+		return;
+	
+	std::vector<int> max, min;
+	max.reserve(n);
+	min.reserve(n/2);
+	for (int i = 0; i + 1 < n; i += 2)
+	{
+		if (v[i] > v[i + 1])
 		{
-			std::istringstream iss(entry.substr(cnt, i - cnt));
-			if (iss >> tmp)
-			{
-				_dNumbers.push_back(tmp);
-				_vNumbers.push_back(tmp);
-			}
-			else
-				throw NegativeInt();
-			if (i + 1 < entry.size())
-				cnt = i + 1;
-			if (i + 1 < entry.size())
-				i++;
+			max.push_back(v[i]);
+			min.push_back(v[i + 1]);
 		}
-		timeUsedS = clock();
-		vMergeList(_vNumbers);
-		timeUsedE = clock();
-		_vTime = static_cast<double>(timeUsedE - timeUsedS) / CLOCKS_PER_SEC * 1000000;
-		timeUsedS = clock();
-		dMergeList(_dNumbers);
-		timeUsedE = clock();
-		_dTime = static_cast<double>(timeUsedE - timeUsedS) / CLOCKS_PER_SEC * 1000000;
-		std::cout << "Time to process a range of " << _nbEntry << " elements with std::vector : " << _vTime << " us." << std::endl;
-		std::cout << "Time to process a range of " << _nbEntry << " elements with std::deque : " << _dTime << " us." << std::endl;
-
+		else
+		{
+			max.push_back(v[i + 1]);
+			min.push_back(v[i]);
+		}
 	}
-}
-
-void	PMergeMe::vMergeList(std::vector<int> &v) {
-	std::vector<std::pair<int, int>> afterFJ;
-	std::vector<int> res;
-
-	vCreatePairs(vec);
-	afterFJ = vFordJohnsonSort(_vPairs);
-	for (std::vector<std::pair<int, int>>::iterator it = afterFJ.begin(); it != afterFJ.end(); ++it)
+	if (n % 2)
+	min.push_back(v[n - 1]);
+	
+	vFordJohnson(max);
+	
+	std::vector<int> insertOrder = vGenerateJacobsthalIndices(min.size());
+	for (size_t i = 0; i < insertOrder.size(); ++i)
 	{
-		res.push_back(it->first);
+		int index = insertOrder[i];
+		if (index < (int)min.size())
+		vBinaryInsert(max, min[index]);
 	}
-	for (std::vector<std::pair<int, int>>::iterator it = afterFJ.begin())
+	v = max;
 }
 
-void	PMergeMe::dMergeList(std::deque<int> &d) {
+//////////////////////////////////DEQUE///////////////////////////////
 
+std::deque<int> PMergeMe::dGenerateJacobsthalIndices(int n){
+	std::deque<int> tmp;
+	tmp.push_back(0);
+	tmp.push_back(1);
+	while (tmp[tmp.size() - 1] < n)
+	{
+		int next = tmp[tmp.size() - 1] * 2 + tmp[tmp.size() - 2];
+		tmp.push_back(next);
+	}
+	tmp.pop_back();
+
+	std::deque<int> index;
+	for (size_t i = 0; i < tmp.size(); i++)
+		index.push_back(tmp[i]);
+	
+	std::deque<bool> used(n, false);
+	for (size_t i = 0; i < tmp.size(); i++)
+		if (index[i] < n)
+			used[index[i]] = true;
+
+	for (int i = 0; i < n; i++)
+		if (!used[i])
+			index.push_back(i);
+	return (index);
+}
+
+void	PMergeMe::dBinaryInsert(std::deque<int> &d, int value){
+	int left = 0, right = d.size();
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		if (d[mid] < value)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+	d.push_back(0);
+	for (int i = (d.size() - 1); i > left; --i)
+		d[i] =  d[i - 1];
+	d[left] = value;
+}
+
+void	PMergeMe::dFordJohnson(std::deque<int> &d){
+	int n = d.size();
+	if (n <= 1)
+		return;
+	
+	std::deque<int> max, min;
+
+	for (int i = 0; i + 1 < n; i += 2)
+	{
+		if (d[i] > d[i + 1])
+		{
+			max.push_back(d[i]);
+			min.push_back(d[i + 1]);
+		}
+		else
+		{
+			max.push_back(d[i + 1]);
+			min.push_back(d[i]);
+		}
+	}
+	if (n % 2)
+	min.push_back(d[n - 1]);
+	
+	dFordJohnson(max);
+	
+	std::deque<int> insertOrder = dGenerateJacobsthalIndices(min.size());
+	for (size_t i = 0; i < insertOrder.size(); ++i)
+	{
+		int index = insertOrder[i];
+		if (index < (int)min.size())
+		dBinaryInsert(max, min[index]);
+	}
+	d = max;
 }
